c23e72daff48ce120a8b8082b106bb9f2b6a16fb
danielCE023 <danic8518@gmail.com>
2025-08-31 12:14:41 -0600

    primerCommit

---
+++ DocParadigmas/prueba.c
@@ -0,0 +1,29 @@
+# include<stdio.h>
+int i;
+int main(){
+
+    /*for ( i; i <= 1; i++)
+    {
+        printf("hola1\n");
+    }*/
+
+    /*for ( i; i <= 1; ++i)
+    {
+        printf("hola2\n");
+    }*/
+
+    /*for ( i; i++<=1;)
+    {
+        printf("hola3\n");
+    }*/
+// este solo imprime una vez
+    for ( i; ++i<=1;)
+    {
+        printf("hola4\n");
+    }
+    
+    
+    
+
+    
+}
---
+++ codigo2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+ int main()
+ {
+         char c;
+
+         c=0xff;
+         if(c==255)
+         {
+                 printf("Verdadero\n");
+         }
+         else
+         {
+                 printf("Falso\n");
+         } 
+
+
+         return 0;
+ }
